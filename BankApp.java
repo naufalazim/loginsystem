ab736b6c357eadd1f749debb2b0a71f364477586
Naufal Azim <mohd.naufal.azim@gmail.com>
2021-06-14 02:24:32 +0800

    Welcome Page change to BankApp

---
+++ BankApp.java
@@ -0,0 +1,24 @@
+import java.awt.Font;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class BankApp {
+	
+	JFrame frame = new JFrame();
+	JLabel welcomeLabel = new JLabel ("Ini Bank App Page");
+	
+	BankApp(String userID){
+		
+		welcomeLabel.setBounds(0,0,350,35);
+		welcomeLabel.setFont(new Font(null,Font.BOLD,25));
+		welcomeLabel.setText("Assalamualaikum "+userID);
+		
+		
+		frame.add(welcomeLabel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(420,420);
+		frame.setLayout(null);
+		frame.setVisible(true);
+	}
+
+}
